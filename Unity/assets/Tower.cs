3e5857e3e04d5df9ec2cf0e97e2b608d85c17e15
Unknown <vuurenskoen@gmail.com>
2017-11-21 16:40:07 +0100

    First push part 1/12

---
+++ Unity/assets/Tower.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour {
+    public float health;
+    public Tower target;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
